f0809bee6d4ad4efd20eac479d818d5ae3f0dcf2
Qayyumaq786 <54434814+Qayyumaq786@users.noreply.github.com>
2024-02-21 13:29:15 +0530

    icons added in sidebar by qayyum

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./SideBar.css";
 import { Link } from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import SourceIcon from '@mui/icons-material/Source';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import DataThresholdingIcon from '@mui/icons-material/DataThresholding';
+import CategoryIcon from '@mui/icons-material/Category';
+import CampaignIcon from '@mui/icons-material/Campaign';
 
 export default function SideBar({ removeCookie, navigate }) {
   return (
@@ -11,41 +18,41 @@ export default function SideBar({ removeCookie, navigate }) {
 
       <div className="linksContainer">
         <div className="links">
-        <Link to={"/dashboard/homepage"} className="buttonLink">
-          <div className="buttonContainer">
-            <span>Home Page</span>{" "}
-          </div>
-        </Link>
-        <Link to={"/dashboard/popularpicks/"} className="buttonLink">
-          <div className="buttonContainer">
-            <span>Popular Picks</span>{" "}
-          </div>
-        </Link>
-        <Link to={"/dashboard/"} className="buttonLink">
-          <div className="buttonContainer">
-            <span>Resource Management</span>{" "}
-          </div>
-        </Link>
-        <Link to={"/dashboard/material/"} className="buttonLink">
-          <div className="buttonContainer">
-            <span> Material Management</span>{" "}
-          </div>
-        </Link>
-        <Link to={"/dashboard/invoices/"} className="buttonLink">
-          <div className="buttonContainer">
-            <span>Invoices</span>
-          </div>
-        </Link>
-        <Link to={"/dashboard/placepromotion"} className="buttonLink">
-          <div className="buttonContainer">
-            <span>Place Promotion</span>
-          </div>
-        </Link>
-        <Link to={"/dashboard/datareport/"} className="buttonLink">
-          <div className="buttonContainer">
-            <span>Data Report</span>{" "}
-          </div>
-        </Link>
+          <Link to={"/dashboard/homepage"} className="buttonLink">
+            <div className="buttonContainer">
+              <span><HomeIcon /> Home Page</span>{" "}
+            </div>
+          </Link>
+          <Link to={"/dashboard/popularpicks/"} className="buttonLink">
+            <div className="buttonContainer">
+              <span> <WhatshotIcon /> Popular Picks</span>{" "}
+            </div>
+          </Link>
+          <Link to={"/dashboard/"} className="buttonLink">
+            <div className="buttonContainer">
+              <span> <SourceIcon /> Resource Management</span>{" "}
+            </div>
+          </Link>
+          <Link to={"/dashboard/material/"} className="buttonLink">
+            <div className="buttonContainer">
+              <span> <CategoryIcon /> Material Management</span>{" "}
+            </div>
+          </Link>
+          <Link to={"/dashboard/invoices/"} className="buttonLink">
+            <div className="buttonContainer">
+              <span> <ReceiptIcon /> Invoices</span>
+            </div>
+          </Link>
+          <Link to={"/dashboard/placepromotion"} className="buttonLink">
+            <div className="buttonContainer">
+              <span> <CampaignIcon /> Place Promotion</span>
+            </div>
+          </Link>
+          <Link to={"/dashboard/datareport/"} className="buttonLink">
+            <div className="buttonContainer">
+              <span> <DataThresholdingIcon /> Data Report</span>{" "}
+            </div>
+          </Link>
         </div>
       </div>
 
