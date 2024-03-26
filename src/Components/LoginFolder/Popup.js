86c337158e4dad077edeabcc33a4eb62c0c9d014
Qayyumaq786 <qayyumaq786@email.com>
2024-03-26 16:42:51 +0530

    popup Maded By Qayyum

--- src/Components/LoginFolder/Popup.js
+++ src/Components/LoginFolder/Popup.js
@@ -3,18 +3,25 @@ import React from 'react';
 import './Checkout.css';
 // images
 
+import { useNavigate } from 'react-router-dom';
+
 const PopUp = props => {
     // function that takes boolean as param to conditionally display popup
-    const { setPopUp } = props 
+    const { setPopUp } = props
+    const navigate = useNavigate();
 
     return (
         <div className="PopUp">
             <div className="pu-content-container">
-                <h1>You have been sucessfully registered!</h1>
+                <h3>We have successfully sent a verification link on your mail please verify then login </h3>
             </div>
             {/* button controls */}
             <div className="pu-button-container">
-                <button onClick={()=> setPopUp(false)}>OK</button>
+                <button onClick={() => {
+                setPopUp(false);
+                navigate('/');
+                }
+                }>OK</button>
             </div>
         </div>
     );
